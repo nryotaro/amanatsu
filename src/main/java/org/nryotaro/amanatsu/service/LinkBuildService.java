9e97cfe527a0bcbac5793f2be77387494d19409e
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-03-13 09:32:50 +0900

    add url

--- src/main/java/org/nryotaro/amanatsu/service/LinkBuildService.java
+++ src/main/java/org/nryotaro/amanatsu/service/LinkBuildService.java
@@ -1,11 +1,19 @@
 package org.nryotaro.amanatsu.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import java.net.URI;
 import java.net.URL;
 import java.time.LocalDateTime;
 
 
+@Service
 public class LinkBuildService {
+
+    @Value("${edgar.url}")
+    private String edgarUrl;
+
     public URI buildIndexLink(LocalDateTime s) {
         return null;
     }
--- src/test/java/org/nryotaro/amanatsu/service/LinkBuildServiceTest.java
+++ src/test/java/org/nryotaro/amanatsu/service/LinkBuildServiceTest.java
@@ -1,18 +1,27 @@
 package org.nryotaro.amanatsu.service;
 
 
-import org.hamcrest.Matchers;
-import org.junit.Assert;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nryotaro.amanatsu.Edgar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = Edgar.class,
+        initializers = ConfigFileApplicationContextInitializer.class)
 public class LinkBuildServiceTest {
-
 
 
     @MockBean
@@ -20,9 +29,14 @@ public class LinkBuildServiceTest {
 
     @Test
     public void testBuildIndexLink() throws Exception {
+        LocalDateTime.parse("2007-12-03T10:15:30");
 
-        new LocalDateTime();
-        Assert.assertThat("", service.buildIndexLink(""), Matchers.equalTo(""));
+        String date = "2016-08-16";
+
+        //default, ISO_LOCAL_DATE
+        LocalDate localDate = LocalDate.parse(date);
+
+        //assertThat("", LocalDateTime.parse("2011-12-03", ), equalTo(""));
 
     }
 }
