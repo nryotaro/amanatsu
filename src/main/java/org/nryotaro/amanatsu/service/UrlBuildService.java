5bbe0074915bf2f29666531db50af7654c0b2875
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-03-14 09:32:09 +0900

    add test

--- src/main/java/org/nryotaro/amanatsu/service/UrlBuildService.java
+++ src/main/java/org/nryotaro/amanatsu/service/UrlBuildService.java
@@ -2,19 +2,18 @@ package org.nryotaro.amanatsu.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.net.URI;
-import java.net.URL;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-
 @Service
-public class LinkBuildService {
+public class UrlBuildService {
 
     @Value("${edgar.url}")
     private String edgarUrl;
 
-    public URI buildIndexLink(LocalDateTime s) {
-        return null;
+    public URI buildIndexLink(LocalDate date) {
+
+            throw new UnsupportedOperationException();
     }
 }
--- src/test/java/org/nryotaro/amanatsu/service/LinkBuildServiceTest.java
+++ src/test/java/org/nryotaro/amanatsu/service/LinkBuildServiceTest.java
@@ -1,42 +1,34 @@
 package org.nryotaro.amanatsu.service;
 
-
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.nryotaro.amanatsu.Edgar;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.net.URI;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest("edgar.url=https://www.sec.gov/")
+/*
 @ContextConfiguration(classes = Edgar.class,
         initializers = ConfigFileApplicationContextInitializer.class)
+        */
 public class LinkBuildServiceTest {
 
+    @Autowired
+    UrlBuildService service;
 
-    @MockBean
-    private LinkBuildService service;
+    String url = "https:/www.sec.gov/";
 
     @Test
     public void testBuildIndexLink() throws Exception {
-        LocalDateTime.parse("2007-12-03T10:15:30");
-
-        String date = "2016-08-16";
-
-        //default, ISO_LOCAL_DATE
-        LocalDate localDate = LocalDate.parse(date);
-
-        //assertThat("", LocalDateTime.parse("2011-12-03", ), equalTo(""));
-
+        assertThat(service.buildIndexLink(LocalDate.parse("2016-08-16")),
+                equalTo(new URI(url + "Archives/edgar/daily-index/2017/QTR1/crawler.20170310.idx")));
     }
+
 }
