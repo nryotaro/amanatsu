5435b7ab5a607a5908ad2836c4527e100aa5f2a6
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-04-20 15:08:52 +0900

    boot spring

--- src/main/kotlin/org/nryotaro/Kotolin.kt
+++ src/main/kotlin/org/nryotaro/Kotolin.kt
@@ -1,9 +1,28 @@
 package org.nryotaro
 
-/**
- * Created by nryotaro on 2017/04/20.
- */
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
 
-fun main(args: Array<String>): Unit {
-    println("Hello world!")
+import org.springframework.boot.SpringApplication
+import org.springframework.stereotype.Service
+
+@Service
+class Sv {
+   val c = 1
+}
+
+@SpringBootApplication
+open class Foo(@Autowired sv: Sv) : CommandLineRunner {
+
+    override fun run(vararg p0: String?) {
+        println("foo")
+        //throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
+
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Foo::class.java, *args)
 }
