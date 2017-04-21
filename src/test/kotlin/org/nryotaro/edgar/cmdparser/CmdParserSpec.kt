115e75fcd1a56e067117575ce0e0409af88d119c
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-04-21 09:39:56 +0900

    deno

---
+++ src/test/kotlin/org/nryotaro/edgar/cmdparser/CmdParserSpec.kt
@@ -0,0 +1,35 @@
+package org.nryotaro.edgar.cmdparser
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.*
+//import kotlin.test.assertEquals
+//import kotlin.test.todo
+
+import org.junit.Assert.assertEquals
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+
+//@RunWith(JUnitPlatform::class)
+object CmdParserSpec: Spek({
+    describe("a calculator") {
+
+        on("addition") {
+            val sum = 2+4
+
+            it("should return the result of adding the first number to the second number") {
+                assertEquals(6, sum)
+            }
+        }
+
+        on("subtraction") {
+            val subtract =  4-5
+
+            it("should return the result of subtracting the second number from the first number") {
+                assertEquals(2, subtract)
+            }
+        }
+    }
+})
