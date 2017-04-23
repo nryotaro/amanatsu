ff900972a6bc80259e6a40ae6312adcdacee750b
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-04-23 11:43:29 +0900

    remove settings related to spek

--- src/test/kotlin/org/nryotaro/edgar/cmdparser/CmdParserTest.kt
+++ src/test/kotlin/org/nryotaro/edgar/cmdparser/CmdParserTest.kt
@@ -1,17 +1,11 @@
 package org.nryotaro.edgar.cmdparser
 
 import org.apache.commons.cli.Options
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
-import org.jetbrains.spek.api.dsl.*
 //import kotlin.test.assertEquals
 //import kotlin.test.todo
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.platform.runner.JUnitPlatform
 import org.junit.runner.RunWith
 import org.nryotaro.edgar.Foo
 import org.springframework.beans.factory.annotation.Autowired
@@ -24,7 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest
 //@ExtendWith(SpringExtension::class)
 @Import(Foo::class)
-class DemoApplicationTests {
+class CmdParserTest {
 
     @Autowired
     lateinit var  options: Options
@@ -36,20 +30,3 @@ class DemoApplicationTests {
     }
 
 }
-
-/*
-//@RunWith(JUnitPlatform::class)
-class CmdParserSpec: Spek({
-    describe("CmdParser") {
-
-        on("takes only a date") {
-            val d = CmdParser.parse("-d" , "2016-12-12", "-o", "~/")
-            val sum = 2+4
-
-            it("should return the result of adding the first number to the second number") {
-                assertEquals(6, sum)
-            }
-        }
-    }
-})
-*/
