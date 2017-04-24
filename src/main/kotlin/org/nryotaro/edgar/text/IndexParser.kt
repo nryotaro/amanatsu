e06f8776dfe7395405612ed63235e9ee00ecda56
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-04-24 15:09:57 +0900

    define extract date method

--- src/main/kotlin/org/nryotaro/edgar/text/IndexParser.kt
+++ src/main/kotlin/org/nryotaro/edgar/text/IndexParser.kt
@@ -13,24 +13,14 @@ class IndexParser {
     private val pattern = DateTimeFormatter.ofPattern("MMM dd, uuuu", Locale.US)
 
     fun parse(index: String) :String {
-       val lines =  index.split(Regex("\\r?\\n"))
-       var date: LocalDate?
-
-       lines.forEach(fun(line: String) {
-
-       })
-
-       lines.forEach {
-           println(it)
-           if(it.startsWith("Last Data Received:")) {
-               val dateStr: String?= Regex("Last Data Received:\\s+(\\w.+)").find(it)?.groupValues?.get(1)
-               date = if(dateStr != null) LocalDate.parse(dateStr, pattern) else null
-               return@forEach
-           }
-           if(it.startsWith("-----------")) {
-           }
-       }
-
+        val lines =  index.split(Regex("\\r?\\n"))
+        val c = extractDate(lines)
        return TODO()
+    }
+
+    private fun extractDate(lines: List<String>): LocalDate {
+        val dateLine: String = lines.first { it.startsWith("Last Data Received:") }
+        val dateStr: String? = Regex("Last Data Received:\\s+(\\w.+)").find(dateLine)?.groupValues?.get(1)
+        return if(dateStr != null) LocalDate.parse(dateStr, pattern) else throw EdgarException("failed to find date in \"$dateLine\"")
     }
 }
