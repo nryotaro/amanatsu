69cdaa94b10ae71e09d15d84f07676834f0b9704
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-04-24 14:52:06 +0900

    parse date

--- src/main/kotlin/org/nryotaro/edgar/text/IndexParser.kt
+++ src/main/kotlin/org/nryotaro/edgar/text/IndexParser.kt
@@ -1,27 +1,33 @@
 package org.nryotaro.edgar.text
 
 import org.nryotaro.edgar.exception.EdgarException
+import org.springframework.cglib.core.Local
 import org.springframework.stereotype.Service
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 @Service
 class IndexParser {
 
-    private val pattern = DateTimeFormatter.ofPattern("MMM dd, uuuu")
+    private val pattern = DateTimeFormatter.ofPattern("MMM dd, uuuu", Locale.US)
 
     fun parse(index: String) :String {
        val lines =  index.split(Regex("\\r?\\n"))
        var date: LocalDate?
 
+       lines.forEach(fun(line: String) {
+
+       })
+
        lines.forEach {
            println(it)
            if(it.startsWith("Last Data Received:")) {
                val dateStr: String?= Regex("Last Data Received:\\s+(\\w.+)").find(it)?.groupValues?.get(1)
-               println(dateStr)
-
                date = if(dateStr != null) LocalDate.parse(dateStr, pattern) else null
                return@forEach
+           }
+           if(it.startsWith("-----------")) {
            }
        }
 
--- src/test/kotlin/org/nryotaro/edgar/text/IndexParserTest.kt
+++ src/test/kotlin/org/nryotaro/edgar/text/IndexParserTest.kt
@@ -3,7 +3,11 @@ package org.nryotaro.edgar.text
 import org.junit.Test
 import org.nryotaro.edgar.EdgarTest
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.format.datetime.DateFormatter
 import java.nio.file.Files
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class IndexParserTest : EdgarTest() {
@@ -13,6 +17,7 @@ class IndexParserTest : EdgarTest() {
    lateinit var parser: IndexParser
 
    @Test fun parseIndex()  {
+
        val text =  this.javaClass.getResourceAsStream("crawler.20170314.idx").bufferedReader().use { it.readText() }
        parser.parse(text)
    }
