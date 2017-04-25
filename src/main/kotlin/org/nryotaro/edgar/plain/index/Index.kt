532a8b6b0825cffe789e679476c4f0d6304dd5c0
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-04-25 13:51:42 +0900

    imple test parse

--- src/main/kotlin/org/nryotaro/edgar/plain/index/Index.kt
+++ src/main/kotlin/org/nryotaro/edgar/plain/index/Index.kt
@@ -6,5 +6,5 @@ import java.time.LocalDate
 
 data class Index(val companyName: String, val formType: String, val cik: Int, val dateFiled: LocalDate, val url: String)
 
-data class Indices(val LocalDate: LocalDate, val indices: List<Index>)
+data class Indices(val filedDate: LocalDate, val indices: List<Index>)
 
--- src/test/kotlin/org/nryotaro/edgar/text/IndexParserTest.kt
+++ src/test/kotlin/org/nryotaro/edgar/text/IndexParserTest.kt
@@ -1,17 +1,14 @@
 package org.nryotaro.edgar.text
 
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Assert.assertThat
 import org.junit.Test
 import org.nryotaro.edgar.EdgarTest
+import org.nryotaro.edgar.plain.index.Index
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.format.datetime.DateFormatter
-import java.nio.file.Files
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.*
-
 
 class IndexParserTest : EdgarTest() {
-
 
    @Autowired
    lateinit var parser: IndexParser
@@ -19,8 +16,11 @@ class IndexParserTest : EdgarTest() {
    @Test fun parseIndex()  {
 
        val text =  this.javaClass.getResourceAsStream("crawler.20170314.idx").bufferedReader().use { it.readText() }
-       val parsed = parser.parse(text)
+       val indices = parser.parse(text)
 
-       println(parsed)
+       assertThat(indices.filedDate, `is`(LocalDate.parse("2017-03-14")))
+       assertThat(indices.indices,
+               `is`(listOf(Index("1ST SOURCE CORP","DEF 14A", 34782,LocalDate.parse("2017-03-14"),
+              "http://www.sec.gov/Archives/edgar/data/34782/0000034782-17-000039-index.htm"))))
    }
 }
