311925ffb610a30ff402ef2dd58e471d1907dd22
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-04-29 21:25:01 +0900

    add test parser

--- src/test/kotlin/org/nryotaro/edgar/text/FilingDetailparserTest.kt
+++ src/test/kotlin/org/nryotaro/edgar/text/FilingDetailparserTest.kt
@@ -1,7 +1,11 @@
 package org.nryotaro.edgar.text
 
+import org.hamcrest.core.Is.`is`
+import org.junit.Assert.assertThat
 import org.junit.Test
 import org.nryotaro.edgar.EdgarTest
+import org.nryotaro.edgar.plain.filingdetail.Document
+import org.nryotaro.edgar.plain.filingdetail.FilingDetail
 import org.springframework.beans.factory.annotation.Autowired
 
 
@@ -12,8 +16,13 @@ class FilingDetailParserTest: EdgarTest() {
 
     @Test
     fun parse() {
-        val c = parser.parse(readTextFile("0001209191-17-028829-index.htm", FilingDetailParserTest::class))
-        println(c)
+        val details: List<FilingDetail> = parser.parse(readTextFile("0001209191-17-028829-index.htm", FilingDetailParserTest::class))
+        assertThat(details.size, `is`(7))
+        assertThat(details[0].seq , `is`(1))
+        assertThat(details[0].description, `is`("DEF 14A"))
+        assertThat(details[0].document, `is`(Document(filename = "proxy2017.htm", url = "/Archives/edgar/data/34782/000003478217000039/proxy2017.htm")))
+        assertThat(details[0].type, `is`("DEF 14A"))
+        assertThat(details[0].size, `is`(929547))
     }
 
 }
