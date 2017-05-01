4448e8dae10c91d2f9403c16fb52c5abe33c140f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-05-01 12:17:49 +0900

    pas

--- src/test/kotlin/org/nryotaro/edgar/EdgarTest.kt
+++ src/test/kotlin/org/nryotaro/edgar/EdgarTest.kt
@@ -7,17 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Configurable
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
+import org.springframework.context.annotation.*
 import org.springframework.stereotype.Service
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.TestPropertySource
 import org.springframework.test.context.junit4.SpringRunner
 import kotlin.reflect.KClass
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 @ActiveProfiles("ut")
+@Import(EdgarRunnerConfiguration::class)
 abstract class EdgarTest {
     protected  fun readTextFile(file: String, klass: KClass<out Any>): String {
         return klass.java.getResourceAsStream(file)
@@ -27,7 +27,28 @@ abstract class EdgarTest {
 
 @Service
 @Profile("ut")
+@Primary
 class EdgarMock: Edgar {
     override fun execute(vararg args: String) {
+    }
+}
+
+@Configuration
+open class EdgarRunnerConfiguration {
+   @Bean
+   @MainRunner
+   open fun edgar(): Edgar {
+       return EdgarService()
+   }
+}
+
+class ATest: EdgarTest() {
+
+    @Autowired
+    @MainRunner
+    lateinit var edgar: Edgar
+
+    @Test fun t() {
+        println("foo")
     }
 }
