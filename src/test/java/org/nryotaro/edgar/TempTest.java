112cf82512b8d57494e5e6efd88c25ce204f7c2c
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-05-01 11:49:04 +0900

    pass test

--- src/test/java/org/nryotaro/edgar/TempTest.java
+++ src/test/java/org/nryotaro/edgar/TempTest.java
@@ -1,14 +1,7 @@
 package org.nryotaro.edgar;
 
-import com.sun.xml.internal.ws.client.sei.ResponseBuilder;
-import org.junit.Test;
-import org.springframework.core.ResolvableType;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.core.io.buffer.DataBufferFactory;
-import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.web.reactive.function.BodyExtractors;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 public class TempTest {
 
--- src/test/kotlin/org/nryotaro/edgar/EdgarTest.kt
+++ src/test/kotlin/org/nryotaro/edgar/EdgarTest.kt
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Configurable
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Service
 import org.springframework.test.context.ActiveProfiles
@@ -28,16 +29,5 @@ abstract class EdgarTest {
 @Profile("ut")
 class EdgarMock: Edgar {
     override fun execute(vararg args: String) {
-    }
-}
-
-@Configurable
-class EdgarBootstrapTest: EdgarTest() {
-
-
-
-    @Test
-    fun execute() {
-        EdgarService().execute()
     }
 }
