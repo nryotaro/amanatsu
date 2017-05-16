1ae9be4ca8f2945b49e58fd14f0d58ec185dd1e1
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-05-17 08:33:33 +0900

    move tests

--- src/main/kotlin/org/nryotaro/edgar/client/EdgarClient.kt
+++ src/main/kotlin/org/nryotaro/edgar/client/EdgarClient.kt
@@ -1,120 +1,15 @@
 package org.nryotaro.edgar.client
 
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Configurable
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Bean
-import org.springframework.core.io.buffer.DataBuffer
-import org.springframework.core.io.buffer.DefaultDataBuffer
-import org.springframework.core.io.buffer.DefaultDataBufferFactory
-import org.springframework.http.client.reactive.ClientHttpRequest
-import org.springframework.http.client.reactive.ReactorClientHttpConnector
-import org.springframework.stereotype.Service
-import org.springframework.web.reactive.function.BodyExtractors
-import org.springframework.web.reactive.function.client.*
-import reactor.core.Disposable
-import reactor.core.publisher.Flux
+import org.springframework.web.reactive.function.client.ClientResponse
 import reactor.core.publisher.Mono
-import reactor.ipc.netty.http.client.HttpClientException
-import java.io.File
-import java.nio.channels.FileChannel
-import java.nio.file.OpenOption
 import java.nio.file.Path
-import java.nio.file.StandardOpenOption
-import java.time.Duration
-import java.time.Duration.ofMillis
-import java.time.Duration.ofSeconds
-import java.util.function.Function
 
-@Configurable
-class EdgarClientContext {
-
-    @Bean fun client(@Value("\${url.root}") edgarRootUrl: String): WebClient {
-        return WebClient
-                .builder()
-                .defaultHeader("Connection", "keep-alive")
-                .defaultHeader("Accept-Encoding", "gzip, deflate")
-                .baseUrl(edgarRootUrl).build()
-    }
-
-}
 
 interface EdgarClient {
+
     fun getRawResponse(url: String): Mono<ClientResponse>
+
     fun get(url: String): Mono<String>
-    fun getBin(url: String): Mono<DataBuffer>
+
     fun download(url: String, path: Path): Mono<Boolean>
-}
-
-@Service
-class EdgarClientImpl(
-        val client: WebClient,
-        @Value("\${url.root}") val edgarRootUrl: String): EdgarClient {
-
-    val log = LoggerFactory.getLogger(this::class.java)
-
-    override fun getRawResponse(url: String): Mono<ClientResponse> {
-       return client.get().uri(url).exchange().timeout(ofSeconds(7L)).retry(3).onErrorResume {
-           log.warn("failed to get \"$url\" caused by $it")
-           Mono.empty()
-       }
-    }
-
-    override fun get(url: String): Mono<String> {
-        return Mono.just(url).delayElement(ofMillis(100L)).flatMap {
-            client.get().uri(it).exchange().timeout(ofSeconds(7L)).onErrorResume {
-                log.warn("failed to get \"$url\" caused by $it")
-                Mono.empty()
-            }.flatMap{
-                it.bodyToMono(String::class)
-            }.doOnNext{log.debug("downloaded: $url")}
-        }
-    }
-
-    /**
-     * TODO Path -> file
-     */
-    override fun download(url: String, path: Path): Mono<Boolean> {
-
-        val chan = FileChannel.open(path, StandardOpenOption.WRITE)
-
-        var failure = false
-
-        return client.get().uri(url).exchange().timeout(ofSeconds(7L))
-                .onErrorResume {
-                    log.warn("failed to download \"$url\" due to $it")
-                    Mono.empty()
-                }.flatMapMany {
-                    it.body(BodyExtractors.toDataBuffers())
-        }.doOnNext {
-            chan.write(it.asByteBuffer())
-        }.doOnError {
-            log.error("""Failed to write "$url" to $path""")
-            if(!failure) {
-                chan.close()
-                path.toFile().delete()
-            }
-            failure = true
-        }.doOnNext {
-            if(chan.isOpen){
-                log.debug("""$url was successfully downloaded""")
-                chan.close()
-            }
-        }.then(Mono.just(!failure))
-    }
-
-    private fun cutEdgarRootUrl(url: String): String {
-        return url.substringAfter(edgarRootUrl)
-    }
-
-    /**
-     * TODO write tests
-     */
-    @Deprecated("not used")
-    override fun getBin(url: String): Mono<DataBuffer> {
-        return client.get().uri(url).exchange().flatMapMany {
-            it.body(BodyExtractors.toDataBuffers())
-        }.reduce{acc, b -> acc.write(b)}
-    }
-
 }
--- src/test/kotlin/org/nryotaro/edgar/Experiment.kt
+++ src/test/kotlin/org/nryotaro/edgar/Experiment.kt
@@ -37,41 +37,5 @@ class Experiment: EdgarTest() {
 
     fun f() {
 
-        val cli = WebClient.create("http://www.sec.gov")
-        val da = LocalDate.parse("2017-03-14")
-        val ff = File("/tmp/hoge")
-        val i: Flux<Index> = indexRetriever.retrieve(da, ff, false).flatMapIterable { it.indices }
-                //.filter{it.url != "http://www.sec.gov/Archives/edgar/data/740629/9999999997-17-002782-index.htm"}
-        //f1(da, ff)
-
-        //val e: Flux<Index> = Flux.just(*i.toIterable().toList().toTypedArray())
-
-        //(val companyName: String, val formType: String, val cik: Int, val dateFiled: LocalDate, val url: String)
-        val ii = Flux.just(Index("comp", "form", 1,LocalDate.now(), "http://www.sec.gov/Archives/edgar/data/1420522/9999999997-17-002867-index.htm"),
-                Index("comp", "form", 1,LocalDate.now(), "http://www.sec.gov/Archives/edgar/data/740629/9999999997-17-002782-index.htm"))
-        val c  =ii.delayElements(Duration.ofMillis(100000L)).
-                flatMap{
-                    //cli.get().uri(it.url).exchange().onErrorResume { Mono.empty() }
-                    repository.retrieve(it, ff,false)
-                }
-
-        val latch = CountDownLatch(1)
-        c.doOnNext { log.debug("next: $it") }.doOnComplete {
-            latch.countDown()
-        }.subscribe {  }
-        latch.await()
     }
-
-    fun f1(da: LocalDate, ff: File): Flux<Index> {
-        return indexRetriever.retrieve(da,
-                ff, false).flatMapIterable { it.indices }.filter{it.url != "http://www.sec.gov/Archives/edgar/data/740629/9999999997-17-002782-index.htm" && it.url != "http://www.sec.gov/Archives/edgar/data/1420522/9999999997-17-002867-index.htm"}
-    }
-    //
-
-    fun temp() {
-
-
-
-    }
-
 }
