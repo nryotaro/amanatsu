c9862199ee3165cb7f563663e866313eb561f8d2
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2017-06-04 16:14:35 +0900

    #1 handle filing detail html file which contains two tables

--- src/main/kotlin/org/nryotaro/edgar/text/FilingDetailParser.kt
+++ src/main/kotlin/org/nryotaro/edgar/text/FilingDetailParser.kt
@@ -1,6 +1,7 @@
 package org.nryotaro.edgar.text
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 import org.nryotaro.edgar.exception.EdgarException
 import org.nryotaro.edgar.plain.filingdetail.Document
@@ -18,6 +19,19 @@ class FilingDetailParser {
     val digit = Regex("\\d+")
 
     fun parse(filingDetail: String): List<FilingDetail> {
+        val tables: Elements = Jsoup.parse(filingDetail).select("div#formDiv table.tableFile")
+
+        val parsed  = tables.map{parseTable(it)}
+        val parsedNotNull = parsed.filterNotNull()
+
+        if(parsed.size != parsedNotNull.size) {
+            throw EdgarException("\"${filingDetail}\": unexpected format")
+        }
+
+        return parsedNotNull.flatMap { it }
+
+
+        /*
         val trs = Jsoup.parse(filingDetail).select("div#formDiv table.tableFile tr")
 
         val headers = trs.first().select("th")
@@ -40,7 +54,33 @@ class FilingDetailParser {
                     type = tds[3].text(),
                     size = i(tds[4].text()))
         }
+        */
+    }
 
+    private fun parseTable(elem: Element): List<FilingDetail>? {
+
+        val trs = elem.select("tr")
+
+        val headers = trs.first().select("th")
+        if(!(headers[0].text() == "Seq" &&
+                headers[1].text() == "Description" &&
+                headers[2].text() == "Document" &&
+                headers[3].text() == "Type" &&
+                headers[4].text() == "Size" &&
+                headers.size == 5)) {
+            return null
+        }
+        val i = {i: String -> if(i.matches(digit)) Integer.parseInt(i) else null}
+
+        return (1..trs.size-1).map {
+            val tds = trs[it].select("td")
+            FilingDetail(
+                    seq = i(tds[0].text()),
+                    description = tds[1].text(),
+                    document =  Document(url = tds[2].select("a").first().attr("href"), filename = tds[2].text()),
+                    type = tds[3].text(),
+                    size = i(tds[4].text()))
+        }
     }
 
 }
--- src/test/kotlin/org/nryotaro/edgar/text/FilingDetailparserTest.kt
+++ src/test/kotlin/org/nryotaro/edgar/text/FilingDetailparserTest.kt
@@ -25,4 +25,23 @@ class FilingDetailParserTest: EdgarTest() {
         assertThat(details[0].size, `is`(929547))
     }
 
+    @Test
+    fun parseTwoTables() {
+
+        val details: List<FilingDetail> = parser.parse(readTextFile("0001564590-17-004283-index.htm", FilingDetailParserTest::class))
+        assertThat(details.size, `is`(20))
+
+        assertThat(details[0].seq , `is`(1))
+        assertThat(details[0].description, `is`("10-K"))
+        assertThat(details[0].document, `is`(Document(filename = "akao-10k_20161231.htm", url = "/Archives/edgar/data/1301501/000156459017004283/akao-10k_20161231.htm")))
+        assertThat(details[0].type, `is`("10-K"))
+        assertThat(details[0].size, `is`(4132994))
+
+        assertThat(details[14].seq , `is`(14))
+        assertThat(details[14].description, `is`("XBRL INSTANCE DOCUMENT"))
+        assertThat(details[14].document, `is`(Document(filename = "akao-20161231.xml", url = "/Archives/edgar/data/1301501/000156459017004283/akao-20161231.xml")))
+        assertThat(details[14].type, `is`("EX-101.INS"))
+        assertThat(details[14].size, `is`(3306989))
+    }
+
 }
